ee8e3ed2ed2b8014a506b533eb9501ba0469090d
FcoMdz <al289539@edu.uaa.mx>
2023-04-23 17:37:14 -0600

    página principal dinámica con impágenes de prueba

---
+++ src/app/components/principal/principal.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'boot-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent {
+  nombres:string [] = [
+    "../../../assets/images/img1.png",
+    "../../../assets/images/img2.jpg",
+    "../../../assets/images/img3.jpg",
+    "../../../assets/images/img4.png",
+    "../../../assets/images/img5.jpg",
+    "../../../assets/images/img6.jpg",
+    "../../../assets/images/img7.png",
+    "../../../assets/images/img8.png",
+    "../../../assets/images/img9.jpg",
+    "../../../assets/images/img10.jpg"
+  ];
+
+}
