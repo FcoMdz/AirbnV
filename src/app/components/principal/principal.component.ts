9975acba8baefa017ec940ac58ee3eee4aa91259
FcoMdz <al289539@edu.uaa.mx>
2023-04-24 10:45:49 -0600

    servicio de casas y agregado de casas dinámico

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CasasService } from 'src/app/services/casas.service';
 
 @Component({
   selector: 'boot-principal',
@@ -6,17 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent {
-  nombres:string [] = [
-    "../../../assets/images/img1.png",
-    "../../../assets/images/img2.jpg",
-    "../../../assets/images/img3.jpg",
-    "../../../assets/images/img4.png",
-    "../../../assets/images/img5.jpg",
-    "../../../assets/images/img6.jpg",
-    "../../../assets/images/img7.png",
-    "../../../assets/images/img8.png",
-    "../../../assets/images/img9.jpg",
-    "../../../assets/images/img10.jpg"
-  ];
 
+  casas:any [];
+ constructor (public casasService: CasasService){
+    this.casas = casasService.casas;
+ }
 }
---
+++ src/app/services/casas.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CasasService {
+  casas:Casa [] = [
+    
+    { id: 1,
+      nombre: "Casa 1",
+      precio:100000,
+      rutaImg:"../../assets/images/img1.png",
+      descripcion:"Esta casa tiene bastante que ofrecer"
+     }
+  
+  ];
+  constructor() { }
+
+ 
+}
+interface Casa {
+  id:number;
+  nombre:string;
+  precio:number;
+  rutaImg:string;
+  descripcion:string;
+}
