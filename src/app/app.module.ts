a09c16ad32e881a3d118be45fd8bb7b81497a943
Gerardo Femat Delgado <fematdelgado.github@gmail.com>
2023-04-27 11:27:11 -0600

    Correccion de errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './components/principal/principal.component';
-<<<<<<< HEAD
+
 import { CasasService } from './services/casas.service';
-=======
+
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { FormsModule } from '@angular/forms';
@@ -20,7 +20,6 @@ import { AnimateModule } from 'primeng/animate';
 import { Checkbox, CheckboxModule } from 'primeng/checkbox';
 import { DropdownModule } from 'primeng/dropdown';
 
->>>>>>> f7d0cbc445c06c9d5510de3f1b0cfbe74692e98f
 
 @NgModule({
   declarations: [
