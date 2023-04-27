33272628e7ad450b872af0f95ab86314f33f9a5e
sandy02star <al292930@edu.uaa.mx>
2023-04-26 20:39:57 -0600

    agregar pagina de registro

---
+++ src/app/components/registro/registro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent {
+    fondo='linear-gradient(135deg, #71b7e6, #9b59b6)';
+}
