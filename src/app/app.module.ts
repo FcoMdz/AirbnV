9a0be6edd855aa5f14fcb4d891a4b52a4e8cac61
Gerardo Femat Delgado <fematdelgado.github@gmail.com>
2023-05-08 19:38:33 -0600

    Merge branch 'casas-vista-individual' into dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { Input, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './components/registro/registro.component';
-
-import { CasasService } from './services/casas.service';
-
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { CasaComponent } from './components/casa/casa.component';
+import { CasasService } from './services/casas.service';
+import { PrincipalComponent } from './components/principal/principal.component';
+
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//ngPrime
 import { SliderModule } from 'primeng/slider';
 import { CalendarModule } from 'primeng/calendar';
 import { InputNumberModule } from 'primeng/inputnumber';
@@ -19,13 +24,13 @@ import { TagModule } from 'primeng/tag';
 import { AnimateModule } from 'primeng/animate';
 import { Checkbox, CheckboxModule } from 'primeng/checkbox';
 import { DropdownModule } from 'primeng/dropdown';
-import { CasaComponent } from './components/casa/casa.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroComponent
+    RegistroComponent,
     PrincipalComponent,
     BusquedaComponent,
     CardsComponent,
