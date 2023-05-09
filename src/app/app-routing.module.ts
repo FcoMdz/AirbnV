cbbaf71c4d8d952e837f8b95ac6d05c269c9f097
FcoMdz <al289539@edu.uaa.mx>
2023-05-08 20:34:31 -0600

    cambios para comenzar a terminar esto Co-authored-by: GERARDO FEMAT DELGADO <al244371@edu.uaa.mx>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { CasaComponent } from './components/casa/casa.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
   { path: 'busqueda/:termino', component: BusquedaComponent},
+  { path: 'busqueda/:termino/:casa', component: CasaComponent}, // con esto corregí lo que pasaba en el buscador, pero no me gusta :C 
   { path: 'busqueda', component: BusquedaComponent},
+  { path: 'registro',  component: RegistroComponent},
   {path: ':casa', component: CasaComponent},
   { path: '', component: PrincipalComponent }
 ];
