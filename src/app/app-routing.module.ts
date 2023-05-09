822d8e45fbb929d83f9c933aa6c4ea0980dfc3ea
FcoMdz <118087778+FcoMdz@users.noreply.github.com>
2023-05-08 18:51:53 -0600

    Revert "página principal dinámica con impágenes de prueba"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PrincipalComponent } from './components/principal/principal.component';
 
-const routes: Routes = [
-  { path: '', component: PrincipalComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
