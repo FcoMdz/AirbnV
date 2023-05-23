b919bb37d7fe98c1a1c8b828a4491dc248c81168
Emmanuel Munoz <emunozce80@gmail.com>
2023-05-22 22:39:52 -0600

    porfiyajala

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,6 +2,12 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-workbox.routing.registerRoute(
-    new workbox.strategies.CacheFirst()
-)
+workbox.loadModule('workbox-strategies');
+
+self.addEventListener('fetch', event => {
+    if (event.request.url.endsWith('.png')) {
+        // Referencing workbox.strategies will now work as expected.
+        const cacheFirst = new workbox.strategies.CacheFirst();
+        event.respondWith(cacheFirst.handle({ request: event.request }));
+    }
+});
